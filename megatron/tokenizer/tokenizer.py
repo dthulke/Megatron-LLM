ab92540bf5a60ca7dd14175e1e32b385191ecd9e
David Thulke <thulke@hltpr.rwth-aachen.de>
2024-11-25 14:19:10 +0100

    fixes llama3 padding token

--- megatron/tokenizer/tokenizer.py
+++ megatron/tokenizer/tokenizer.py
@@ -603,7 +603,7 @@ class _TikTokenTokenizer(AbstractTokenizer):
         # BOS / EOS token IDs
         self.bos_id: int = self.special_tokens["<|begin_of_text|>"]
         self.eos_id: int = self.special_tokens["<|end_of_text|>"]
-        self.pad_id: int = -1
+        self.pad_id: int = 128004
         self.stop_tokens = {
             self.special_tokens["<|end_of_text|>"],
             self.special_tokens["<|eot_id|>"],
